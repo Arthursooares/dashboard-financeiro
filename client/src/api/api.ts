d12c3ef17571b427d9814a257bc1e62978e77b56
Arthursooares <hardlinixxsioares@gmail.com>
2026-04-07 10:41:33 -0300

    chore: configura frontend para usar backend em produção

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://dashboard-financeiro-api-fug5.onrender.com",
 });
 
 export default api;
